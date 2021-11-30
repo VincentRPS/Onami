224fcf4e698a02603dedba1907b7ddb7724c51bc
RPS <vincentbusiness55@gmail.com>
2021-11-29 20:38:52 -0800

    Fix Desc

--- onami/features/slash_commands.py
+++ onami/features/slash_commands.py
@@ -2,7 +2,7 @@
 onami.features.slash_commands
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Slash Command Support For Onami
+Slash Command Features For Onami
 
 :copyright: (c) 2021-present RPS
 :license: MIT, see LICENSE for more details.
