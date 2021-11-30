0cfd7b56f2fa142c6dabb2e29307f669497079f9
RPS <vincentbusiness55@gmail.com>
2021-11-29 20:37:01 -0800

    Start Working On Slash Commands

---
+++ onami/features/slash_commands.py
@@ -0,0 +1,12 @@
+"""
+onami.features.slash_commands
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Slash Command Support For Onami
+
+:copyright: (c) 2021-present RPS
+:license: MIT, see LICENSE for more details.
+"""
+
+import nextcord
+from nextcord.ext import *
