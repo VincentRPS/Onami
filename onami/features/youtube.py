560cb0ed60a5d00e1ab304f787701c68c8a586b3
Vincent <vincentbusiness55@gmail.com>
2022-02-14 12:16:30 +0800

    feat: replace youtube_dl with yt-dlp

--- onami/features/youtube.py
+++ onami/features/youtube.py
@@ -12,7 +12,7 @@ The onami youtube-dl command.
 """
 
 import nextcord
-import youtube_dl
+import yt_dlp
 from nextcord.ext import commands
 
 from onami.features.baseclass import Feature
@@ -27,11 +27,11 @@ BASIC_OPTS = {
 
 class BasicYouTubeDLSource(nextcord.FFmpegPCMAudio):
     """
-    Basic audio source for youtube_dl-compatible URLs.
+    Basic audio source for yt_dlp-compatible URLs.
     """
 
     def __init__(self, url, download: bool = False):
-        ytdl = youtube_dl.YoutubeDL(BASIC_OPTS)
+        ytdl = yt_dlp.YoutubeDL(BASIC_OPTS)
         info = ytdl.extract_info(url, download=download)
         super().__init__(info['url'])
 
@@ -44,7 +44,7 @@ class YouTubeFeature(Feature):
     @Feature.Command(parent="oni_voice", name="youtube_dl", aliases=["youtubedl", "ytdl", "yt"])
     async def oni_vc_youtube_dl(self, ctx: commands.Context, *, url: str):
         """
-        Plays audio from youtube_dl-compatible sources.
+        Plays audio from yt-dlp-compatible sources.
         """
 
         if await VoiceFeature.connected_check(ctx):
